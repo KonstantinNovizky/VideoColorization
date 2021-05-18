9774a1952f254b0f8fa0eac000995870cf503585
KonstantinNovizky <konstantinnovizky@gmail.com>
2021-05-18 08:05:54 +0700

    fixed

--- getModels.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/richzhang/colorization/blob/8aa25ac7b6215bf6789e682b6fa3433164d4a3a4/resources/pts_in_hull.npy?raw=true -O ./pts_in_hull.npy
-wget http://people.eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2.caffemodel -O ./models/colorization_release_v2.caffemodel
-wget http://people.eecs.berkeley.edu/~rich.zhang/projects/2016_colorization/files/demo_v2/colorization_release_v2_norebal.caffemodel -O ./models/colorization_release_v2_norebal.caffemodel
